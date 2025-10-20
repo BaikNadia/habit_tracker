8fbc6a77df3868d15624bd3fffa930f8535db865
BaikNadia <baikowa.nadia@gmail.com>
2025-10-20 10:16:49 +0300

    change serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -4,11 +4,18 @@ from rest_framework import serializers
 User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(
+        write_only=True,
+        style={'input_type': 'password'}  # Это поможет Swagger понять тип поля
+    )
 
     class Meta:
         model = User
         fields = ('email', 'password', 'telegram_chat_id')
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'required': True, 'write_only': True}
+        }
 
     def create(self, validated_data):
         user = User.objects.create_user(
